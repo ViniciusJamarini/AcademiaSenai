95ef32a31625de97feb2f4f546749f03b9bd23f6
Vinicius Jamarini <vinicius_jamarini@estudante.sc.senai.br>
2022-03-17 20:31:06 -0300

    repost

--- src/br/com/senai/academiasenai/entity/Equipamento.java
+++ src/br/com/senai/academiasenai/entity/Equipamento.java
@@ -1,5 +1,38 @@
 package br.com.senai.academiasenai.entity;
 
 public class Equipamento {
+	
+	private Integer id;
+	private String descricao;
+	
+	public Equipamento() {
+		super();
+	}
+
+		public Equipamento(Integer id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
 
 }
