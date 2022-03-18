3ea238ee21e765d1289e744c874b125627bc214b
Vinicius Jamarini <vinicius_jamarini@estudante.sc.senai.br>
2022-03-17 21:32:08 -0300

    update

--- src/br/com/senai/academiasenai/entity/Professor.java
+++ src/br/com/senai/academiasenai/entity/Professor.java
@@ -1,5 +1,31 @@
 package br.com.senai.academiasenai.entity;
 
+import java.time.LocalDate;
+
 public class Professor {
-	private int idade;
-}
+
+	private Integer id;
+	private String nome;
+	private int ano;
+	
+	public Professor(Integer id, String nome, LocalDate dataDeNascmineto) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}	
